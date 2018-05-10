f89962c32df74d95204a62025a9241a5ef220431
Anthony Stanton <anthony@contentful.com>
2018-05-10 09:27:00 +0200

    Use correct secret for Github

--- roles/git/templates/github.sh
+++ roles/git/templates/github.sh
@@ -1,2 +1,3 @@
 export GITHUB_USER={{ lookup('lastpass', 'github.com - elblivion', field='username') }}
-export GITHUB_TOKEN={{ lookup('lastpass', 'github.com - elblivion', field='token') }}
+export GITHUB_PASSWORD={{ lookup('lastpass', 'github.com - elblivion', field='token') }}
+export GITHUB_TOKEN={{ lookup('lastpass', 'Github https token', field='password') }}
